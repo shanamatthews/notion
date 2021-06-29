ffb61aa1ec4d1fa366ba35954e95b4f9452aa636
Shana Matthews <shana.l.matthews@gmail.com>
2021-06-29 13:14:10 -0700

    add page content

--- newrwxboostpage.js
+++ newrwxboostpage.js
@@ -6,25 +6,54 @@ const databaseId = process.env.NOTION_DATABASE_ID
 
 // get args from CLI
 const argv = minimist(process.argv.slice(2));
-const pageTitle = argv._[0];
-const videoLink = argv._[1];
+const pageTitle1 = argv._[0];
+const pageTitle2 = argv._[1];
+const pageTitle = pageTitle1 + pageTitle2;
+const videoLink = argv._[2];
 
-async function addItem(text) {
+async function addItem(title, videoLink) {
   try {
     await notion.pages.create({
       parent: { database_id: databaseId },
       properties: {
-        Name: { title: [{ text: { "content": text } }] },
+        Name: { title: [{ text: { "content": title } }] },
         Class: { select: { name: "rwx-boost" } },
         Type: { select: { name: "Lecture" } },
         "Video link": { url: videoLink },
-      }
+        // this is broken - can't find an example in docs
+        // Materials: { files: [{ name: videoLink }] },
+      },
+      children: [{
+        object: "block",
+        type: "paragraph",
+        paragraph: {
+          text: [{
+            type: "text",
+            text: {
+              content: "rwx boost course page",
+              link: { url: "https://github.com/rwxrob/boost" },
+            }
+          }],
+        }
+      },
+      {
+        object: "block",
+        type: "heading_1",
+        heading_1: {
+          text: [{
+            type: "text",
+            text: {
+              content: "Day " + pageTitle2
+            },
+          }],
+        },
+      }],
     }).then(response => {
-      console.log(videoLink);
+      // console.log(response);
     });
   } catch (error) {
     console.error(error.body)
   }
 }
 
-addItem(pageTitle);
+addItem(pageTitle, videoLink);
